f0da262e1cd2598907db69e39fcf3b818f03bbab
mizosuke <mizosuke.dev@gmail.com>
2022-05-11 18:03:52 +0900

    [#22] Add unit testing code for streamIDGen.

---
+++ proxy/idgen_test.go
@@ -0,0 +1,36 @@
+package proxy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type StreamIDGenTest struct {
+	suite.Suite
+}
+
+func TestStreamIDGen(t *testing.T) {
+	suite.Run(t, new(StreamIDGenTest))
+}
+
+// TestNormal confirm the operation when streamIDGen is used normally.
+func (suite *StreamIDGenTest) TestNormal() {
+
+	idGen := newStreamIDGen()
+
+	idMap := map[int32]struct{}{}
+
+	for i := 0; i < maxCounter; i++ {
+		id := idGen.generate()
+		idMap[id] = struct{}{}
+	}
+
+	suite.Require().Len(idMap, maxCounter)
+
+	id := idGen.generate()
+
+	// check counter reset.
+	counter := id / 1000000
+	suite.Require().Equal(1, counter)
+}
