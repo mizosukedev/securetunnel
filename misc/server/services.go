a2339b828cbc3fdb8b571eb9760e5122afd040d5
mizosuke <mizosuke.dev@gmail.com>
2022-07-04 19:50:35 +0900

    [#66] Add common processing for WebAPI.

--- misc/server/services.go
+++ misc/server/services.go
@@ -1,5 +1,53 @@
 package server
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Validatable interface {
+	Validate() error
+}
+
+func PreProcess[T any](handler func(*gin.Context, T)) gin.HandlerFunc {
+
+	wrapHandler := func(ctx *gin.Context) {
+
+		// new(T) -> **OpenTunnelRequest
+		args := new(T)
+
+		err := ctx.Bind(args)
+		if err != nil {
+			message := fmt.Sprintf("failed to parse request: %v", err)
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": message})
+			return
+		}
+
+		err = ctx.BindHeader(args)
+		if err != nil {
+			message := fmt.Sprintf("failed to parse header: %v", err)
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": message})
+			return
+		}
+
+		validatable, ok := any(*args).(Validatable)
+		if ok {
+			err = validatable.Validate()
+			if err != nil {
+				message := fmt.Sprintf("failed to validate request: %v", err)
+				ctx.JSON(http.StatusBadRequest, gin.H{"message": message})
+				return
+			}
+		}
+
+		handler(ctx, *args)
+	}
+
+	return wrapHandler
+}
+
 type Services struct {
 	NeedAuth bool
 	Auth     Authorizer
